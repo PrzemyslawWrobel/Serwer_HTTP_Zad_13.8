80a515c0b737bd924f085aef6fb86570a938a76f
Przemysław Wróbel <przemek.wrobel@poczta.fm>
2018-03-22 23:42:14 +0100

    DRY

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer();
 
+function read(path) {
+    fs.readFile(path, 'utf-8', function (err, data) {
+            if (err) throw err;
+            console.log('Connected');
+            response.write(data);
+            response.end();
+        });
+}
 server.on('request', function (request, response) {
 	if (request.method === 'GET' && request.url === '/') {
-        fs.readFile('./index.html', function (err, data) {
-            if (err) throw err;
-            console.log('Connected');
-            response.write(data);
-            response.end();
-        });
+        read('./index.html');
+                /*fs.readFile('./index.html', function (err, data) {
+                    if (err) throw err;
+                    console.log('Connected');
+                    response.write(data);
+                    response.end();
+                });*/
 
    }else if (request.method === 'GET' && request.url === '/pytanie') {
-        fs.readFile('./pytanie.txt', 'utf-8', function (err, data) {
-            if (err) throw err;
-            console.log('Connected');
-            response.write(data);
-            response.end();
-        });
+        read('./pytanie.txt');
+
+                /*fs.readFile('./pytanie.txt', 'utf-8', function (err, data) {
+                    if (err) throw err;
+                    console.log('Connected');
+                    response.write(data);
+                    response.end();
+                });*/
 
 
 
     } else {
-
         response.statusCode = 404;
-        fs.readFile('./404.jpg', 'utf-8', function (err, data) {
-            if (err) throw err;
-            console.log('Wrong path');
-            response.write(data);
-            response.end();
-        });
+        read('./404.jpg');
+                /*fs.readFile('./404.jpg', 'utf-8', function (err, data) {
+                    if (err) throw err;
+                    console.log('Wrong path');
+                    response.write(data);
+                    response.end();
+                });*/
     }
 });
 
